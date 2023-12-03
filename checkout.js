2fb8abe40e009d8a7a226967246bec1446143fe4
Isaac Afful <isaac.afful@hubtel.com>
2023-12-03 01:08:45 +0000

    Refactor checkout.js file

--- checkout.js
+++ checkout.js
@@ -1,29 +1,7 @@
+const HubtelPay = (function () {
+  let paymentData = {};
 
-var MySDK = function () { 
-  var myApiUrl = "https://payproxyapi.hubtel.com/items/initiate";
-
-   function openModal (data) {
-    createIfram();
-    return fetch(myApiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Basic a09va1BHdjo4MDA2ZTA5NjRkYWI0ZjFmYWEyZGVlOTgyZmMyMWNiMw==",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        renderWebpageInPopup(result.data.checkoutUrl);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        throw error;
-      });
-  };
-
-   function  createIfram () {
+  function createIframe() {
     // Create the iframe element
     let iframe = document.createElement("iframe");
 
@@ -37,9 +15,11 @@ var MySDK = function () {
     );
 
     document.body.appendChild(iframe);
-  };
 
- function renderWebpageInPopup (url) {
+  }
+
+    
+ function renderWebpageInPopup(url) {
     const iframe = document.createElement("iframe");
     iframe.src = url;
     iframe.setAttribute("frameborder", "0");
@@ -52,6 +32,97 @@ var MySDK = function () {
     modal.style.position = "fixed";
     modal.style.top = "50px";
     modal.style.marginLeft = "auto";
-  }
-}
+    modal.style.marginRight = "auto";
+    modal.style.width = "100%";
+    modal.style.maxWidth = "480px";
 
+    modal.style.backgroundColor = "#fff";
+    modal.style.display = "flex";
+    modal.style.alignItems = "center";
+    modal.style.justifyContent = "center";
+    modal.style.zIndex = "65675656565";
+    modal.style.transition = "bottom 0.5s ease"; // Add transition property
+
+    const closeIcon = document.createElement("div");
+    closeIcon.innerHTML = "&times;"; // HTML entity for "times" (close) symbol
+    closeIcon.style.position = "absolute";
+    closeIcon.style.top = "-30px";
+    closeIcon.style.right = "0px";
+    closeIcon.style.cursor = "pointer";
+    closeIcon.style.borderRadius = "50%";
+    closeIcon.style.padding = "0 6px";
+    closeIcon.style.fontSize = "20px";
+    closeIcon.style.color = "#000";
+    closeIcon.style.backgroundColor = "#dedede";
+    closeIcon.addEventListener("click", () => {
+      const iframe = document.getElementById("loaderIframe");
+      console.log(iframe);
+      document.body.removeChild(modal);
+      console.log(modal);
+      document.body.removeChild(iframe);
+    });
+    modal.appendChild(closeIcon);
+
+    // Create loading indicator
+    const loadingIndicator = document.createElement("div");
+    loadingIndicator.textContent = "Loading...";
+    loadingIndicator.style.display = "none"; // Initially hidden
+    // modal.appendChild(loadingIndicator);
+
+    iframe.addEventListener("load", () => {
+      // Hide the loading indicator once the iframe is loaded
+      loadingIndicator.style.display = "none";
+    });
+
+    // Add error event listener to the iframe
+    iframe.addEventListener("error", () => {
+      // Handle iframe load errors here
+      loadingIndicator.textContent = "Error loading content.";
+    });
+
+    modal.appendChild(iframe);
+    document.body.appendChild(modal);
+    modal.offsetWidth;
+
+    loadingIndicator.style.display = "block";
+    modal.style.bottom = "0";
+  }
+
+
+  function openModal() {
+    if (!paymentData.totalAmount) {
+      console.error('Payment data is incomplete');
+      return;
+    }
+
+    createIframe();
+    fetch('https://payproxyapi.hubtel.com/items/initiate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'Basic a09va1BHdjo4MDA2ZTA5NjRkYWI0ZjFmYWEyZGVlOTgyZmMyMWNiMw==',
+      },
+      body: JSON.stringify(paymentData),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('Success:', result);
+        renderWebpageInPopup(result.data.checkoutUrl);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        throw error;
+      });
+  }
+
+  return {
+    setup: function (payload) {
+        paymentData = payload
+      // Return an object with the public API
+      return {
+        openModal: openModal,
+      };
+    },
+  };
+})();
